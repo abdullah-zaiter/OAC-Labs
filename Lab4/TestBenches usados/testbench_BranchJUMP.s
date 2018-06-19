4c97e02517be3abacc33ef0651abf508d1a2a95c
Skira <lucaschiavini@hotmail.com>
2018-06-18 21:17:44 -0300

    Organização das pastas

--- Lab4/TestBenches usados/testbench_BranchJUMP.s
+++ Lab4/TestBenches usados/testbench_BranchJUMP.s
@@ -11,7 +11,15 @@ BEGIN:
 	beq t2, t2, TESTBNE
 	add t2, t3, t2
 TESTBNE:
-	bne t2, t3, END
+	bne t2, t3, TESTBGE
 	addi t2, t3, 4
+TESTBGE:
+	bge t3, t2, TESTBLTU #1
+TESTBGEU:
+	bgeu t3, t2, END # 4
+TESTBLT:
+	blt t2,t3, TESTBGEU # 3
+TESTBLTU:
+	bltu t2,t3, TESTBLT # 2
 END: 
 	j BEGIN
