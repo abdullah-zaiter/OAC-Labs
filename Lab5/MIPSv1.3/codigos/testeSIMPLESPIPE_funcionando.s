0c5489391dd0dc9ba64da93c57d8708ba2576186
jose reinaldo <josereinaldoneto@gmail.com>
2018-06-24 19:06:31 -0300

    Correcao do arquivo testeSIMPLESPIPE_funcionando.s

--- Lab5/MIPSv1.3/codigos/testeSIMPLESPIPE_funcionando.s
+++ Lab5/MIPSv1.3/codigos/testeSIMPLESPIPE_funcionando.s
@@ -1,6 +1,6 @@
 # Teste para verificacao da simulacao por forma de onda e na DE1-SoC
 .data
-	WORD: .word 0xB0DEB0B0, 0xF0CAF0FA, 0x00400070 #0x00400074 
+	WORD: .word 0xB0DEB0B0, 0xF0CAF0FA, 0x00400068 #0x00400074 
 .text
 	lui $t0,0x1001
 	ori $t0,$t0,0x0000
@@ -13,18 +13,15 @@
 	addi $t0,$t0,1		# t0 <- bodebob3
 PULA:	lw $t2,0($t1)		# t2 <- bodebobo
 	bne $t0,$t2,PULA2	
-	nop
 	addi $t0,$t1,-1
 	addi $t0,$t0,-1
 PULA2:  addi $t0,$t0,1		# t0 <- bodebob4
 	jal PROC
-	#nop	#		(testado e funcionando)
 	lw $t2,8($t1)		# t2 <- 0x00400068
+	#nop
 	nop
-	nop
-	nop
+	nop			# funciona com dois nops
 	jr $t2
-	#nop			#(testado e funcionando)
 VOLTA:	addi $t0,$t0,1
 	j FIM1
 	addi $t0,$t0,-1
